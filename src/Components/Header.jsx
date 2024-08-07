664dcd30116de4cc8ae5328adcf0f8f108ee1ee0
Hishmat786 <hishmatdrawarh786@gmail.com>
2024-08-07 19:32:31 +0500

    added home page

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,40 +1,44 @@
 import React from 'react'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function Header() {
-   const NavItems=[
-    {
-        name:'Home',
-        href:'#'
-    },
-    {
-        name:'Products',
-        href:'#'
-    },
-    {
-        name:'Contacts',
-        href:'#'
-    },
-   ]
-  return (
-    <>
-    <nav className="flex justify-between flex-row bg-gray-800 text-white p-2 ">
-        <div className="flex center space-x-2">
-            <img src="src/assets/logo.png" className='w-10 '/>
-            <h2 className='text-xl'>DraShop</h2>
-        </div>
+    const NavItems = [
+        { name: 'Home', href: '#' },
+        { name: 'Products', href: '#' },
+        { name: 'Latest', href: '#' },
+        { name: 'Contacts', href: '#' },
+    ]
+    return (
+        <>
+            <nav className="flex justify-around flex-row bg-gray-800 text-white p-2 ">
+                <div className="flex center space-x-2">
+                    <img src="src/assets/logo.png" className='w-10  ' />
+                    <h2 className='text-4xl'>DraShop</h2>
+                </div>
 
-        <ul className='flex space-x-6 list-none px-3'>
-            {NavItems.map((item,index)=>(
-                <li key={index} className='transition-transform transform hover:scale-110'>
-                    <a href={item.href} className='no-underline hover:'> {item.name}
-                    </a>
-                </li>
-            ))}
-        </ul>    
-  
-    </nav> 
-    </>
-  )
+                <ul className='flex justify-start space-x-6 list-none px-3 py-2 text-xl'>
+                    {NavItems.map((item, index) => (
+                        <li key={index} className='transition-transform transform hover:scale-110'>
+                            <a href={item.href} className='no-underline hover:'> {item.name}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+                <div className="flex flex-row my-1">
+                    <div className="flex flex-row my-1 items-center relative">
+                        <i className="fas fa-search absolute left-3 mx-1 text-gray-400"></i>
+                        <input
+                            type="text"
+                            placeholder="Find Product"
+                            className="rounded-xl pl-10 pr-2 py-2 mx-2 border border-gray-300 text-black w-full"
+                        />
+                    </div>
+                    <i className='fas fa-shopping-cart py-2 mx-3 text-3xl hover:cursor-pointer' ></i>
+                </div>
+
+            </nav>
+        </>
+    )
 }
 
 export default Header
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import Header from './Header'
 
 function Home() {
   return (
-    <>
-        <Header/>
-    </>
+    <div className='flex flex-row justify-around'>
+      <div className='grid place-items-center items-center bg-gradient-to-r text-black p-10 my-12 '>
+        <div className='text-center'>
+
+        <h2 className='text-3xl text-red-500 font-semibold mb-2'>40% Discount</h2>
+        <h1 className='text-5xl font-bold mb-1'>Special Summer</h1>
+        <h1 className='text-5xl font-bold mb-4'>Collection</h1>
+        <p className='text-xl font-light'>Best Collection Of 2024</p>
+        </div>
+      </div>
+      <div>
+        <img src="src/assets/sale.png" alt="" />
+      </div>
+       
+    </div>
   )
 }
 

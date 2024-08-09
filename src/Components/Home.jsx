dc8b3f83bc0ca34a49df5eeb7384b515c7526947
Hishmat786 <hishmatdrawarh786@gmail.com>
2024-08-09 22:54:45 +0500

    added product with api

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
         <div className='text-center'>
 
         <h2 className='text-3xl text-red-500 font-semibold mb-2'>40% Discount</h2>
-        <h1 className='text-5xl font-bold mb-1'>Special Summer</h1>
+        <h1 className='text-5xl font-bold mb-1'>Special Winter</h1>
         <h1 className='text-5xl font-bold mb-4'>Collection</h1>
         <p className='text-xl font-light'>Best Collection Of 2024</p>
         <button className='bg-gray-800 text-white mt-3 m-4 p-3 text-3xl rounded-3xl hover:bg-gray-600'>Shop Now</button>
--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,35 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Products() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(json => {
+        const firstTwentyProducts = json.slice(0, 20);
+        setProducts(firstTwentyProducts);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  function pTitle(t) {
+    console.log(t)
+    console.log("thek")
+    console.log(t.split(' ').slice(0, 2).join(' '))
+
+    return t.split(' ').slice(2).join(' ')
+  }
   return (
     <div className='flex flex-col '>
-        <div className="font-sans">
-            <h1 className='text-4xl font-edu font-bold ml-16 text-center'>Featured Products</h1>
-        </div>
-        <div className='flex flex-row flex-wrap gap-7 mt-8 p-11'>
-            <div className='flex flex-col border-black'>
-                <div>
-                    <img src="src/assets/show.jpg" alt="" className='w-48 h-56' />
-                </div>
-                <div className='flex flex-col items-start ml-3'>
-                    <h1 className='text-xl mt-3'>DNK Yellow Shoes</h1>
-                    <p className='text-s text-red-400'>Men</p>
-                    <p className='font-bold'>1500$</p>
-                    <button className='p-2 mt-3 rounded-3xl font-bold text-white bg-slate-600'>Add To Cart</button>
-                </div>
+      <div className="font-sans">
+        <h1 className='text-4xl font-edu font-bold ml-16 text-center'>Featured Products</h1>
+      </div>
+      <div className='flex flex-row flex-wrap gap-7 mt-8 p-11'>
+        {products.map((product) => (
+          <div key={product.id} className='flex flex-col border border-black rounded-2xl w-56 h-80 overflow-hidden hover:shadow-lg hover:shadow-blue-500/50'>
+            <div className='flex-shrink-0 mt-3'>
+              <img src={product.image} className='w-full h-40 object-contain' />
             </div>
-            <div className='flex flex-col border-black'>
-                <div>
-                    <img src="src/assets/show.jpg" alt="" className='w-48 h-56' />
-                </div>
-                <div className='flex flex-col items-start ml-3'>
-                    <h1 className='text-xl mt-3'>DNK Yellow Shoes</h1>
-                    <p className='text-s text-red-400'>Men</p>
-                    <p className='font-bold'>1500$</p>
-                    <button className='p-2 mt-3 rounded-3xl font-bold text-white bg-slate-600'>Add To Cart</button>
-                </div>
+            <div className='flex flex-col items-start p-3'>
+              <h1 className='text-sm mt-1 w-full truncate'>{product.title.split(' ').slice(2).join(' ')}</h1>
+              <p className='text-xs text-red-400'>{product.category}</p>
+              <p className='font-bold text-sm'>{product.price}$</p>
+              <div className='mt-3 w-full'>
+                <button className='p-2 mt-auto w-full rounded-3xl font-bold text-white bg-slate-600'>Add To Cart</button>
+              </div>
             </div>
-        </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
